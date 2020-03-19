f0cc53118444ce79908952b50b0f21d57b6ccbf7
fidabspd <fidabspd@gmail.com>
2020-03-19 19:03:26 +0900

    200319 self study

--- CODES/sort2.py
+++ CODES/sort2.py
@@ -2,23 +2,9 @@ numbers1 = [6, 10, 2]  # return "6210"
 numbers = [3, 30, 34, 5, 9]  # return "9534330"
 
 
-#print(sorted(numbers, key = lambda num: str(num)[0], reverse = True))
 
-#from functools import reduce
-#origin = numbers
-#length = len(str(max(numbers)))
-#numbers = [str(num) for num in numbers]
-#for idx, num in zip(range(len(numbers)), numbers):
-#    tmp = num + '0' * (length - len(num))
-#    numbers[idx] = tmp
-
-#numbers1 = sorted(numbers, reverse = True)
-#print(numbers)
-#print(sorted(origin, key = lambda x: index))
-
-#print(reduce(lambda x, y: x + y, sorted(numbers, reverse = True)))
-
-def solution(numbers):
+### 알고리즘 틀림
+def solution1(numbers):
     from functools import reduce
     numbers.sort()
     origin = numbers
@@ -26,11 +12,24 @@ def solution(numbers):
     numbers = [str(num) for num in numbers]
     arr = []
     for idx, num in zip(range(len(numbers)), numbers):
-        tmp = num + '0' * (length - len(num))
+        tmp = num + num[-1] * (length - len(num))
         arr.append([origin[idx], tmp])
 
     ans = sorted(arr, key = lambda x: x[1], reverse = True)
     return reduce(lambda x, y: x + y, [str(num) for num, id in ans])
 
+#print(solution1(numbers))
+#print(solution1(numbers1))
+
+
+
+### 알고리즘 틀림
+def solution(numbers):
+    from functools import reduce
+    numbers.sort(reverse = True)
+    return reduce(lambda x, y : str(x) + str(y), sorted(numbers, key = lambda x: str(x) + str(x)[0] * (len(str(max(numbers))) - len(str(x))), reverse = True))
+
 print(solution(numbers))
-print(solution(numbers1))
+print(solution([0,0,0,0]))
+print(solution([12,121]))
+print(solution([31,313]))
--- CODES/test.py
+++ CODES/test.py
@@ -1,3 +1,12 @@
-a = [1,2,3,4]
-a.append(['123', '123'])
-print(a)
+numbers1 = [6, 10, 2]  # return "6210"
+numbers = [3, 30, 34, 5, 9]  # return "9534330"
+
+#a = sorted(numbers, key = lambda x: str(x) + str(x)[-1] * (len(str(numbers[-1])) - len(str(x))), reverse = True)
+#print(list(map(lambda x: str(x) + str(x)[-1] * (len(str(numbers[-1])) - len(str(x))), numbers)))
+#print(a)
+
+#a = lambda x: str(x) + str(x)[-1] * (len(str(numbers[-1])) - len(str(x)))
+#print(a(1))
+
+a = lambda x: str(x) + str(x)[0] * (len(str(max(numbers))) - len(str(x)))
+print(sorted(list(map(a, numbers)), reverse=True))
