ff904451aa2ced628953a293807733b87c74a03e
fidabspd <fidabspd@gmail.com>
2020-07-24 19:08:03 +0900

    200724 self study

--- source/CODES/DFSBFS-네트워크.py
+++ source/CODES/DFSBFS-네트워크.py
@@ -2,9 +2,12 @@ n1 = 3; computers1 = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]  # 2
 n2 = 3; computers2 = [[1, 1, 0], [1, 1, 1], [0, 1, 1]]  # 1
 n3 = 5; computers3 = [[1, 0, 1, 0, 0], [0, 1, 1 ,0, 0], [1, 1, 1, 0, 0], [0, 0, 0, 1, 1], [0, 0, 0, 1, 1]]  # 2
 n4 = 5; computers4 = [[1, 0, 1, 0, 0], [0, 1, 0, 0, 0], [1, 0, 1, 0, 1], [0, 0, 0, 1, 0], [0, 0, 1, 0, 1]]  # 3
-n4 = 3; computers5 = [[1,1,0], [1,1,1], [0,1,1]]  # 1
+n5 = 3; computers5 = [[1,1,0], [1,1,1], [0,1,1]]  # 1
+n6 = 6; computers6 = [[1, 0, 1, 1, 0, 0], [0, 1, 0, 0, 0, 1], [1, 0, 1, 0, 0, 0], [1, 0, 0, 1, 0, 0], [0, 0, 0, 0, 1, 0], [0, 1, 0, 0, 0, 1]]  # 3
 
-def solution(n, computers):
+
+### 틀림
+def solution1(n, computers):
 
     answer = 0
     now = 0
@@ -34,7 +37,47 @@ def solution(n, computers):
         
     return answer
 
-# print(solution(n1, computers1))
-# print(solution(n2, computers2))
-# print(solution(n3, computers3))
+
+
+### 통과
+def solution(n, computers):
+    answer = 0  # 네트워크 총 개수
+
+    while computers:
+        now = 0  # 현재 검사할 com
+        check = []  # node의 끝까지 본것
+        network = []  # 현 network에 포함된 com
+
+        print(f'computers:')
+        for com in computers:
+            print(com)
+        
+        while check != network or not check:  # 현 network에 있는 com과 check를 끝낸 com이 같을때까지 (check가 비어있면 check와 network가 같아도 루프 in)
+            print(f'now: {now}, network: {network}, check: {check}')
+            for idx, com in enumerate(computers[now]):
+                if com == 1 and idx not in network:
+                    network.append(idx)
+            check.append(now)
+
+            for net in network:
+                if net not in check:
+                    now = net
+                    break
+
+        for net in sorted(network, reverse = True):
+            computers.pop(net)
+            for com in computers:
+                com.pop(net)
+
+
+        
+        answer += 1
+        
+    return answer
+
+print(solution(n1, computers1))
+print(solution(n2, computers2))
+print(solution(n3, computers3))
 print(solution(n4, computers4))
+print(solution(n5, computers5))
+print(solution(n6, computers6))
---
+++ source/CODES/DFSBFS-단어 변환.py
@@ -0,0 +1,3 @@
+begin1 = 'hit'; target1 = 'cog'; words1 = ['hot', 'dot', 'dog', 'lot', 'log', 'cog']  # 4
+begin2 = 'hit'; target2 = 'cog'; words2 = ['hot', 'dot', 'dog', 'lot', 'log']  # 0
+
