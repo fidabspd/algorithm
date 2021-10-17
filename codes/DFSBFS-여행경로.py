3a8e70d1ac71067846df04f9cde6106a43800dcd
fidabspd <fidabspd@gmail.com>
2021-10-17 19:25:15 +0900

    211017 algorithm self study

--- codes/DFSBFS-여행경로.py
+++ codes/DFSBFS-여행경로.py
@@ -2,24 +2,28 @@ tickets1 = [['ICN', 'JFK'], ['HND', 'IAD'], ['JFK', 'HND']]  # [ICN, JFK, HND, I
 tickets2 = [['ICN', 'SFO'], ['ICN', 'ATL'], ['SFO', 'ATL'], ['ATL', 'ICN'], ['ATL', 'SFO']]  # [ICN, ATL, ICN, SFO, ATL, SFO]
 
 
-# 틀림
 def solution(tickets):
+    answers = [[i] for i, ticket in enumerate(tickets) if ticket[0] == 'ICN']
     
-    tickets.sort()
-    now = 'ICN'
-    tmp = []
+    while max(list(map(len, answers))) < len(tickets):
+        new_answers = []
+        for answer in answers:
+            for i, ticket in enumerate(tickets):
+                if i not in answer and ticket[0] == tickets[answer[-1]][-1]:
+                    new_answer = answer.copy()
+                    new_answer.append(i)
+                    new_answers.append(new_answer)
+        answers = new_answers
+    
+    answers_str = []
+    for answer in answers:
+        answer_str = ['ICN']
+        for i in answer:
+            answer_str += [tickets[i][1]]
+        answers_str.append(answer_str)
 
-    while tickets:
-        for ticket in tickets:
-            while now not in [ticket[0] for ticket in tickets]:
-                tickets.append(tmp.pop())
-            if ticket[0] == now:
-                tmp.append(tickets.pop(tickets.index(ticket)))
-                now = ticket[1]
-                break
-        print(f'tmp: {tmp}, tickets: {tickets}')
+    return min(answers_str)
 
-    return [t[0] for t in tmp] + [tmp[-1][1]]
 
 print(solution(tickets1))
 print(solution(tickets2))
