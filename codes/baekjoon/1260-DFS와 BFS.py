a979578fac2f93d430c3dad5dd2d39345d404df2
fidabspd <fidabspd@gmail.com>
2021-10-20 00:59:36 +0900

    211020 algorithm self study

--- codes/baekjoon/1260-DFS와 BFS.py
+++ codes/baekjoon/1260-DFS와 BFS.py
@@ -1,16 +1,56 @@
-n1 = 4; m1 = 5; v1 = 1; graphs1 = [[1, 2], [1, 3], [1, 4], [2, 4], [3, 4]]
-n2 = 5; m2 = 5; v2 = 3; graphs2 = [[5, 4], [5, 2], [1, 2], [3, 4], [3, 1]]
-n3 = 1000; m3 = 1; v3 = 1000; graphs3 = [[999, 1000]]
+n1 = 4; m1 = 5; v1 = 1; edge_info1 = [[1, 2], [1, 3], [1, 4], [2, 4], [3, 4]]
+n2 = 5; m2 = 5; v2 = 3; edge_info2 = [[5, 4], [5, 2], [1, 2], [3, 4], [3, 1]]
+n3 = 1000; m3 = 1; v3 = 1000; edge_info3 = [[999, 1000]]
 
 
 from collections import deque
 
-def bfs(n, m, v, graphs):
+def make_graph(edge_info):
+    graph = {}
+    for n1, n2 in edge_info:
+        if n1 not in graph.keys():
+            graph[n1] = [n2]
+        elif n2 not in graph[n1]:
+            graph[n1].append(n2)
+        if n2 not in graph.keys():
+            graph[n2] = [n1]
+        elif n1 not in graph[n2]:
+            graph[n2].append(n1)
+    return graph
+
+def dfs(graph, v):
     visited = []
-    queue = deque()
+    stack = [v]
 
-def dfs(n, m, v, graphs):
-    pass
+    while stack:
+        n = stack.pop()
+        if n not in visited:
+            visited.append(n)
+            if n in graph:
+                tmp = list(set(graph[n]) - set(visited))
+                tmp.sort(reverse=True)  # stack이기 때문에 처음이 마지막으로 오도록.
+                stack += tmp
+    return visited
 
-def solution():
-    pass
+def bfs(graph, v):
+    visited = []
+    queue = deque([v])
+    
+    while queue:
+        n = queue.popleft()
+        if n not in visited:
+            visited.append(n)
+            if n in graph.keys():
+                tmp = list(set(graph[n]) - set(visited))
+                tmp.sort()
+                queue += tmp
+    return visited
+
+def solution(n, m, v, edge_info):
+    graph = make_graph(edge_info)
+    return dfs(graph, v), bfs(graph, v)
+
+
+print(solution(n1, m1, v1, edge_info1))
+print(solution(n2, m2, v2, edge_info2))
+print(solution(n3, m3, v3, edge_info3))
