c310b1b91eaf826aef7bb3fb703d5afd75cafb50
fidabspd <fidabspd@gmail.com>
2021-10-19 23:35:28 +0900

    211019 algorithm self study

---
+++ codes/baekjoon/1260-DFS와 BFS.py
@@ -0,0 +1,16 @@
+n1 = 4; m1 = 5; v1 = 1; graphs1 = [[1, 2], [1, 3], [1, 4], [2, 4], [3, 4]]
+n2 = 5; m2 = 5; v2 = 3; graphs2 = [[5, 4], [5, 2], [1, 2], [3, 4], [3, 1]]
+n3 = 1000; m3 = 1; v3 = 1000; graphs3 = [[999, 1000]]
+
+
+from collections import deque
+
+def bfs(n, m, v, graphs):
+    visited = []
+    queue = deque()
+
+def dfs(n, m, v, graphs):
+    pass
+
+def solution():
+    pass
